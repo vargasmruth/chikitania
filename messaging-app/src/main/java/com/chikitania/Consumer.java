8c4bd450924ae6178d023b88bdc8738b8ba5981f
Ruth Vargas <ruth.vargas@srlpunto.com>
2020-03-28 12:09:37 -0400

    add chikitania event

--- messaging-app/src/main/java/com/chikitania/Consumer.java
+++ messaging-app/src/main/java/com/chikitania/Consumer.java
@@ -1,20 +1,17 @@
 package com.chikitania;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
 import java.util.concurrent.TimeoutException;
 
 /**
  * Created by Ruth on 27/03/2020.
  */
-public class Sender {
+public class Consumer {
     public static void main(String[] args) throws IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
         String uri = System.getenv("CLOUDAMQP_URL");
         if (uri == null) uri = "amqp://nehvmuzt:v-eN1QJeh4AWnZiYVHLs1_NJTT4DLocG@spider.rmq.cloudamqp.com/nehvmuzt";
@@ -23,14 +20,13 @@ public class Sender {
 
         factory.setUri(uri);
 
-        try (Connection connection = factory.newConnection()){
-            Channel channel = connection.createChannel();
-            channel.queueDeclare("q-event", false, false, false, null);
-            String message = "it is a test" + LocalDateTime.now();
-
-            channel.basicPublish("", "q-event", false, null, message.getBytes());
-            System.out.println("message has been send");
-        }
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare("q-event", false, false, false, null);
+        channel.basicConsume("q-event", true, (consumerTag, message) -> {
+            String msg = new String(message.getBody(), "UTF-8");
+            System.out.println("I just recived a message: " + msg);
+        }, consumerTag -> {});
 
 
     }
